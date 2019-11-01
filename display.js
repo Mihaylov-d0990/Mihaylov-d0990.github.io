5f0b598282e856af622905c5f3a304d98e1baf36
Mihaylov-d0990 <54925831+Mihaylov-d0990@users.noreply.github.com>
2019-11-01 23:03:33 +0700

    Update display.js

--- display.js
+++ display.js
@@ -1,1 +1,5 @@
-eval(function(p,a,c,k,e,d){e=function(c){return c.toString(36)};if(!''.replace(/^/,String)){while(c--){d[c.toString(a)]=k[c]||c.toString(a)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('7 1=8(\'Какой пароль?\');6.4(1);2(1==(3 9 a‬*5)){f.e("d")[0].b.c=\'g\'}',17,17,'|result|if|19|log||console|let|prompt|801|760|style|display|body|getElementsByTagName|document|block'.split('|'),0,{}))
+let result = prompt('Какой пароль?');
+console.log(result);
+if (result == 9886){
+    document.getElementsByTagName("body")[0].style.display = 'block';
+}
