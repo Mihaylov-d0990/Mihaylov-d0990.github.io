e66ec209d609781b2029e2798c2032e91ac7b418
Mihaylov-d0990 <54925831+Mihaylov-d0990@users.noreply.github.com>
2019-12-19 10:34:48 +0700

    Update script.js

--- adaptive/script.js
+++ adaptive/script.js
@@ -1,26 +1,28 @@
-function addProject(src, paragraph){
-    let projects = document.getElementById('projects');
+function addProject(){
+	for (let i = 0; i < texts.length; i++){
+		let projects = document.getElementById('projects');
 
-    let projectBox = document.createElement('div');
-    projectBox.className = 'project-box';
+		let projectBox = document.createElement('div');
+		projectBox.className = 'project-box';
 
-    let projectImage = document.createElement('div');
-    projectImage.className = 'project-image';
+		let projectImage = document.createElement('div');
+		projectImage.className = 'project-image';
 
-    let image = document.createElement('img');
-    image.src = src;
+		let image = document.createElement('img');
+		image.src = images[i];
 
-    let projectText = document.createElement('div');
-    projectText.className = 'project-text';
+		let projectText = document.createElement('div');
+		projectText.className = 'project-text';
+	
+		let text = document.createElement('p');
+		text.innerHTML = texts[i];
 
-    let text = document.createElement('p');
-    text.innerHTML = paragraph;
-
-    projectImage.appendChild(image);
-    projectText.appendChild(text);
-    projectBox.appendChild(projectImage);
-    projectBox.appendChild(projectText);
-    projects.appendChild(projectBox);
+		projectImage.appendChild(image);
+		projectText.appendChild(text);
+		projectBox.appendChild(projectImage);
+		projectBox.appendChild(projectText);
+		projects.appendChild(projectBox);
+	}
 }
 
 function check() {
@@ -59,6 +61,8 @@ var y = window.matchMedia("(max-width: 580px)");
 myFunction(y);
 y.addListener(myFunction);
 
+
+
 var texts =     ["Яндекс курьер - это революционная игра с новой игровой механикой. В игре даётся возможность испытать себя в роли бедного студента, зарабатывая гроши. !Спойлер! если выбрать сложность 'Россия', то в конце игры студент умирает от голода.",
                 "Каменщик - это непризнанный шедевр, повествующий нам о тяжёлой судьбе обычного рабочего из Луганска. Сюжет игры начинается с того, что каменщику приходится украсться хлеб, ради спасенения своей жизни от голодной смерти. Я - каменщик, работая три дня, без зарплаты, я не отдам хлеб, я голоден.",
                 "Если вы никогда не угоняли машину, чтобы угнать машину, то вы не слышали об Углепластике. Углепластик - это история о нелёгкой судьбе провинциального работяги, который недавно вернулся из тюрьмы. Главный герой никогда не бросает пацанов с района в тяжёлой жизненной ситуации. О, чёрт, мы снова здесь.",
@@ -72,7 +76,3 @@ var images =    ["https://www.iphones.ru/wp-content/uploads/2019/11/92DF8E4B-9F3
                 "https://steamuserimages-a.akamaihd.net/ugc/943963057793915127/7F97B1DD878D6B0CF793E01D2D7EBD4DAF1D9298/?imw=512&amp;imh=289&amp;ima=fit&amp;impolicy=Letterbox&amp;imcolor=%23000000&amp;letterbox=true",
                 "https://i.pinimg.com/originals/be/1e/77/be1e77301c88240e25ed86510b4801bb.jpg",
                 "https://b1.gmbox.ru/c/122476.jpg"];
-
-for (let i = 0; i < texts.length; i++){
-    addProject(images[i], texts[i]);
-}
